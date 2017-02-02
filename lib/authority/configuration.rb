7dbe29f8e49490d0619f953b2858e105bfbff710
John Poulin <john.m.poulin@gmail.com>
2017-02-01 19:50:57 -0500

    Added option to enable logging for all authority interactions, as per #118
    Added option to enable logging for all authority interactions, as per #118
    
    Logging will utilize Authority.info, and will only occur in instance within which a SecurityViolation is not generated

--- lib/authority/configuration.rb
+++ lib/authority/configuration.rb
@@ -3,7 +3,7 @@ module Authority
 
     # Has default settings, which can be overridden in the initializer.
 
-    attr_accessor :abilities, :controller_action_map, :user_method, :security_violation_handler, :logger
+    attr_accessor :abilities, :controller_action_map, :user_method, :security_violation_handler, :logger, :log_all
 
     def initialize
 
